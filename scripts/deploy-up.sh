f24f289ae31aa3367ab571b4a6a708fdb4f4e3dd
Todd Brunia <tbrunia@gmail.com>
2020-10-20 17:40:51 -0500

    fix: fixed unzipping build file

--- scripts/deploy-up.sh
+++ scripts/deploy-up.sh
@@ -5,14 +5,11 @@ pipelineId=${COYOTE_PIPELINE_ID}
 stageName=${STAGE_NAME}
 
 cd "${buildOutputDir}" || false
-ls
-mkdir ./temp
-mv ./artifacts.zip ./temp
+mkdir -p ./temp
+mv ./build.zip ./temp
 cd ./temp || false
-unzip artifacts.zip
-rm -rf artifacts.zip
-unzip build.zip
-rm -rf build.zip
+unzip ./build.zip
+rm -rf ./build.zip
 stageNameLower=$(echo "${stageName}" | tr '[:upper:]' '[:lower:]')
 bucket="s3://xilution-coyote-${pipelineId:0:8}-${stageNameLower}-web-content"
 aws s3 cp . "${bucket}" --recursive --include "*" --acl public-read
